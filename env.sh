dc966c3b79e13dc8976802ac802df8c59a1048aa
Yuya Yamamoto <yuya.yamamoto@co-design.t.u-tokyo.ac.jp>
2026-08-04 14:49:40 +0000

    envの作成

---
+++ env.sh
@@ -0,0 +1,3 @@
+source "/work/PARC2026_pre/venv/bin/activate"
+export PYTHONPATH="/work/PARC2026_pre/LIBERO-plus:/work/PARC2026_pre:/work/PARC2026_pre/compe"
+export LIBERO_ROOT="/work/PARC2026_pre/LIBERO-plus"
